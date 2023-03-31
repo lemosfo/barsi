0eac0d6c16c7bdb81a8f757f983487608cb350a1
Fernando Lemos <lemosfo2@gmail.com>
2023-03-31 19:39:18 -0300

    fix maven

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject barsi "0.1.0-SNAPSHOT"
-  :description "Service to create financial item"
+  :description "Service to create financial items"
   :url "http://myapp.com/:service-name/api/:route"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
-  :repositories ^:replace [["nu-codeartifact" {:url "https://repo1.maven.org"}]]
+  :repositories {:url "https://repo1.maven.org"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [io.pedestal/pedestal.service "0.5.10"]
                  [io.pedestal/pedestal.immutant "0.5.10"]
@@ -12,4 +12,4 @@
   :profiles {:dev     {:dependencies [[io.pedestal/pedestal.service-tools "0.5.11-beta-1"]
                                       [io.pedestal/pedestal.jetty "0.5.10"]]}
              :uberjar {:aot [tio-patinhas.server]}}
-  :repl-options {:init-ns tio-patinhas.core})
+  :repl-options {:init-ns barsi.core})
