c74d9fcaa7de5529711377d8a46f351afe67f35c
Fernando Lemos <lemosfo2@gmail.com>
2023-10-30 23:54:31 -0300

    Input financial register done

--- test/barsi/cash_flow_test.clj
+++ test/barsi/cash_flow_test.clj
@@ -1,7 +1,6 @@
 (ns barsi.cash-flow-test
   (:require [clojure.test :refer :all]
-            [barsi.logic.cash-flow :as cash-flow]
-            [barsi.db.dev :as db]))
+            [barsi.logic.cash-flow :as cash-flow]))
 
 (def input
   "{\"description\":\"Apple Watch\",
@@ -17,11 +16,10 @@
 (deftest register-financial-input-test
   (testing "return the register of a financial transaction")
   (is (= (-> input
-             (cash-flow/register-financial-input)
-             (filter @db/base)) [{:description "Apple Watch"
-                               :date        "2023-08-29"
-                               :amount      3000.00
-                               :account     "Nubank"
-                               :transaction "Mercado Livre"
-                               :id          "123456"
-                               :flags       ["Eletronics" "Free"]}])))
+             (cash-flow/register-financial-input)) [{:description "Apple Watch"
+                                  :date        "2023-08-29"
+                                  :amount      3000.00
+                                  :account     "Nubank"
+                                  :transaction "Mercado Livre"
+                                  :id          "123456"
+                                  :flags       ["Eletronics" "Free"]}])))
